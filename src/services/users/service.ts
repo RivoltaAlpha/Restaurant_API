a3aab7f2f4ad7193231798aec93bbd9b8fb4518b
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-06-05 22:45:21 +0300

    folder structure

---
+++ src/services/users/service.ts
@@ -0,0 +1,9 @@
+// import { drizzle } from 'drizzle-orm';
+// import { pgClient } from '../drizzle/drizzle.config';
+// import { users } from '../drizzle/schema';
+
+// const db = drizzle(pgClient);
+
+// export async function getAllUsers() {
+//     return await db.select().from(users).execute();
+// }
